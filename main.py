8e2cd71ddc297d5d50a4a5ada48a1b30e9bb78a7
Kill-JoyCoder <ankurgohain2005@gmail.com>
2023-12-13 11:16:59 +0530

    Commit 2@13/12/23

--- main.py
+++ main.py
@@ -1,10 +1,48 @@
-
-#from pytube import Playlist
+import tkinter 
+import customtkinter
 from pytube import YouTube
 
-def video(link):
-    yt = YouTube(link)
-    video = yt.streams.get_highest_resolution()
-    video.download('./Downloads')
-video(input("Enter the link>_ "))
+def dlaudio():
+    try:
+        ytlink= link.get()
+        ytobj = YouTube(ytlink)
+        audio = ytobj.streams.get_audio_only()
+        audio.download('./dl/audio')
+        print("Downloaded Audio")
+    except:
+        print("invalid")
 
+def dlvideo():
+    try:
+        ytlink = link.get()
+        ytobj = YouTube(ytlink)
+        video = ytobj.streams.get_highest_resolution()
+        video.download('./dl/video')
+        print("Downloaded Video")
+    except:
+        print("Invalid")
+    
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("green")
+
+#app frame
+app = tkinter.Tk()
+app.geometry("720x480")
+app.title("TattleTube")
+
+#UI elem
+title = customtkinter.CTkLabel(app, text="Link", width=100, height=20, text_color="black")
+title.pack(padx=10, pady=10)
+
+#input
+url_var = tkinter.StringVar()
+link = customtkinter.CTkEntry(app, width=350, height=40,textvariable=url_var)
+link.pack()
+
+#download
+dl = customtkinter.CTkButton(app, text='Download Audio', command=dlaudio)
+dl.pack(padx=10, pady=10)
+dl = customtkinter.CTkButton(app, text='Download Video', command=dlvideo)
+dl.pack(padx=10, pady=10)
+#runs app
+app.mainloop()
